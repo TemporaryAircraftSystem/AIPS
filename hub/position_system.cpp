9d9fb61831697801bab426220120a83408b68ac3
Anton Fedotov <fedotov.anton.af@gmail.com>
2016-02-05 14:31:40 +0300

    initial. add camera position detection

---
+++ hub/position_system.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anton on 04.02.16.
+//
+
+#include "position_system.h"
---
+++ hub/position_system.hpp
@@ -0,0 +1,15 @@
+//
+// Created by anton on 04.02.16.
+//
+
+#ifndef AIPS_POSITION_SYSTEM_H
+#define AIPS_POSITION_SYSTEM_H
+
+
+class position_system
+{
+
+};
+
+
+#endif //AIPS_POSITION_SYSTEM_H
